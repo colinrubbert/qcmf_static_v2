2b85fcbb1f768256dbba3f54b56d52ed43f3523b
Colin Rubbert <colinrubbert@gmail.com>
2016-07-11 10:53:05 -0500

    style contact section for desktop and footer for desktop

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ function mobileNavigationToggle() {
     $('#mobile-navigation').slideToggle(300);
   });
 }
+
+
+// Function to display Google Maps location
+function initMap() {
+  var myLatLng = {lat: 44.20382, lng: -88.4934747};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'QCMF'
+  });
+}
