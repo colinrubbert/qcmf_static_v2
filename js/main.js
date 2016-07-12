30b2a0ed5aee74230370bfd65e9c1b9b6b738206
Colin Rubbert <colinrubbert@gmail.com>
2016-07-12 10:57:11 -0500

    Gallery page build, styling, and jquery

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // jQuery
 $(function(){
   mobileNavigationToggle();
+  galleryAlbumSwitcher();
 });
 
 // Function to slide open and close the mobile navigation menu
@@ -8,6 +9,34 @@ function mobileNavigationToggle() {
   $('.navigation-mobile-toggle').click(function() {
     $('#mobile-navigation').slideToggle(300);
   });
+}
+
+// Function to choose which gallery to view
+function galleryAlbumSwitcher() {
+  $('#carbon-steel').click(function(){
+    $('#carbon-steel').addClass('active');
+    $('#stainless-steel').removeClass('active');
+    $('#aluminum').removeClass('active');
+    $('#carbon-steel-album').show();
+    $('#stainless-steel-album').hide(300);
+    $('#aluminum-album').hide(300);
+  })
+  $('#stainless-steel').click(function(){
+    $('#stainless-steel').addClass('active');
+    $('#carbon-steel').removeClass('active');
+    $('#aluminum').removeClass('active');
+    $('#carbon-steel-album').hide(300);
+    $('#stainless-steel-album').show();
+    $('#aluminum-album').hide(300);
+  })
+  $('#aluminum').click(function(){
+    $('#stainless-steel').removeClass('active');
+    $('#carbon-steel').removeClass('active');
+    $('#aluminum').addClass('active');
+    $('#carbon-steel-album').hide(300);
+    $('#stainless-steel-album').hide(300);
+    $('#aluminum-album').show(300);
+  })
 }
 
 
